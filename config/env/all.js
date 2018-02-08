d7c520f63197274b60699d46238d8a398f366d46
Stefan Streichsbier <stefan@streichsbier.at>
2018-02-08 11:25:30 +0700

    Add database configuration

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 // default app configuration
 module.exports = {
     port: process.env.PORT || 4000,
-    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URI || "mongodb://hackmeplease:xxx@ds159217.mlab.com:59217/hackmeplease",
-    cookieSecret: "session_cookie_secret_key_here",
-    cryptoKey: "a_secure_key_for_crypto_here",
+    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URI || "mongodb://hackmeplease:hackmeplease@ds127888.mlab.com:27888/hackmeplease",
+    cookieSecret: "123123123123123la;sdk;asmdas;ld",
+    cryptoKey: "lasd;asdm3lkmamsdamsdaksd;lasm;d",
     cryptoAlgo: "aes256",
     hostName: "localhost"
 };
